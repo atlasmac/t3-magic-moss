2dfc31d386cedddd069706339d32ba7c199d15ab
atlasmac <mckinleyatlas@gmail.com>
2023-02-22 10:44:46 -0700

    add red and green pin ternary

--- src/components/MapMarker.tsx
+++ src/components/MapMarker.tsx
@@ -1,9 +1,12 @@
-import React from "react";
+import { useState } from "react";
 import L from "leaflet";
 import { getLatLon } from "../helpers/getLatLon";
 import { getLocation } from "../helpers/getLocation";
 import { Marker, Popup } from "react-leaflet";
 import Link from "next/link";
+import { api } from "../utils/api";
+import { PulseLoader } from "react-spinners";
+import { getConditions } from "../helpers/getConditions";
 
 interface Props {
   siteId: string;
@@ -13,12 +16,45 @@ interface Props {
 function MapMarker({ siteId, siteName }: Props) {
   const position = getLatLon(siteId);
   const location = getLocation(siteId);
-  const icon = L.icon({
-    iconUrl: "/pin.png",
-    iconSize: [20, 20],
-    iconAnchor: [10, 20], // point of the icon which will correspond to marker's location
-    popupAnchor: [0, -15],
-  });
+
+  const [fetched, setFetched] = useState<boolean>(false);
+
+  const current = api.forecast.getCurrentLevel.useQuery(
+    {
+      siteId,
+    },
+    {
+      onSuccess() {
+        setFetched(true);
+      },
+    }
+  );
+  const currentLevel = current.data?.observation
+    .filter((e) => {
+      if (e.cfs > 0) {
+        return e;
+      }
+    })
+    .pop();
+
+  const conditions = getConditions([[currentLevel?.cfs || 0, siteId]]);
+
+  console.log(conditions);
+
+  const icon =
+    conditions[0] === "Flat" || conditions[0] === "Poor"
+      ? L.icon({
+          iconUrl: "/redPin.png",
+          iconSize: [20, 20],
+          iconAnchor: [10, 20], // point of the icon which will correspond to marker's location
+          popupAnchor: [0, -15],
+        })
+      : L.icon({
+          iconUrl: "/greenPin.png",
+          iconSize: [20, 20],
+          iconAnchor: [10, 20], // point of the icon which will correspond to marker's location
+          popupAnchor: [0, -15],
+        });
 
   return (
     <Marker position={position} icon={icon}>
@@ -30,6 +66,18 @@ function MapMarker({ siteId, siteName }: Props) {
           </div>
           <div>
             View on <Link href={location}>Google Maps</Link>
+          </div>
+          <div>
+            {" "}
+            {fetched ? (
+              <>Currently at {currentLevel?.cfs} cfs</>
+            ) : (
+              <PulseLoader
+                color="rgb(166,173, 187)"
+                size={4}
+                speedMultiplier={0.5}
+              />
+            )}
           </div>
         </div>
       </Popup>
