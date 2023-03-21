08fddbe28f347e42904d7db0a85c443b8fce4afc
atlasmac <mckinleyatlas@gmail.com>
2023-03-21 11:22:08 -0600

    remove sql

--- prisma/migrations/20230321171047_add_unique_constraint_cfs/migration.sql
+++ prisma/migrations/20230321171047_add_unique_constraint_cfs/migration.sql
@@ -2,7 +2,7 @@
   Warnings:
 
   - A unique constraint covering the columns `[siteId,condition,cfs]` on the table `DetailedReport` will be added. If there are existing duplicate values, this will fail.
-  - A unique constraint covering the columns `[siteId,condition,cfs]` on the table `RiverConditions` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[siteId,cfs]` on the table `RiverConditions` will be added. If there are existing duplicate values, this will fail.
 
 */
 -- DropIndex
@@ -11,8 +11,8 @@ DROP INDEX "DetailedReport_siteId_condition_key";
 -- DropIndex
 DROP INDEX "RiverConditions_siteId_condition_key";
 
--- CreateIndex
+CreateIndex
 CREATE UNIQUE INDEX "DetailedReport_siteId_condition_cfs_key" ON "DetailedReport"("siteId", "condition", "cfs");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "RiverConditions_siteId_condition_cfs_key" ON "RiverConditions"("siteId", "condition", "cfs");
+CREATE UNIQUE INDEX "RiverConditions_siteId_cfs_key" ON "RiverConditions"("siteId", "cfs");
