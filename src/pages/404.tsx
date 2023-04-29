1199927e9077a97321402b8741877df28b415c0c
atlasmac <mckinleyatlas@gmail.com>
2023-04-28 22:48:40 -0600

    make 404 look better on mobile

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,7 +5,7 @@ function NotFound() {
     <>
       <Layout>
         <div className="flex h-[85vh] items-center justify-center">
-          <p className="text-5xl">Oops! There&apos;s nothing here.</p>
+          <p className="text-2xl w-[80vw] sm:text-5xl">Oops! There&apos;s nothing here.</p>
         </div>
       </Layout>
     </>
